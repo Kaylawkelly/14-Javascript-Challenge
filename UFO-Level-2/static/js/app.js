a64e81591cf2f8bfba5dd5a9827f93d10b35d757
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-04-28 19:36:36 -0400

    commit

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -5,3 +5,6 @@ var tableData = data;
 
 //Reference
 var $tbody =d3.select("tbody")
+var button = d3.select("#filter-btn")
+var inputFieldDate = d3.select("#datetime")
+var inputFieldCity = d3.select("#city")
